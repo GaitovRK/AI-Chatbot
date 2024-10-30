3930e0ffdc5d9990029b08dd1d9217cceb6cd5e5
Rashid <rashidgaitov@gmail.com>
2024-10-30 18:58:14 +0300

    Initial Commit

---
+++ AI Mentor/AI_MentorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_MentorApp.swift
+//  AI Mentor
+//
+//  Created by Rashid Gaitov on 30.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct AI_MentorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
